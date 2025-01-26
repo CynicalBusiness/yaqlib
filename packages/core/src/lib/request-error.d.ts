c3e1fe9fe45e77d39cecaaba92136496c03da203
CynicalBusiness <matt@vevox.io>
2025-01-26 23:55:31 +0100

    cleanup accidental .d.ts files that snuck in

--- packages/core/src/lib/request-error.d.ts
+++
@@ -1,5 +0,0 @@
-import { RequestResponse } from "./request.types.js";
-export declare class RequestError extends Error {
-    readonly response: RequestResponse;
-    constructor(response: RequestResponse, options?: ErrorOptions);
-}
